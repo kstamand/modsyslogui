06338ddfdb0c754b1e3f2a5a030b8145444929ac
kstamand <166230847+kstamand@users.noreply.github.com>
2025-12-10 17:18:10 -0500

    Update customfunction.js

--- customfunction.js
+++ customfunction.js
@@ -1,7 +1,7 @@
-//+++ Start Custom Asuswrt UI log filter code\
+//++ Start Custom Asuswrt UI log filter code\
 function filterHandler(){\
 event\
 .preventDefault();\
 setTimeout(get_log_data, 100);\
 }\
-//+++ End Custom Asuswrt UI log filter code
+//++ End Custom Asuswrt UI log filter code
